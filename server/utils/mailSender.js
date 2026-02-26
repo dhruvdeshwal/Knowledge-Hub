9a7c76a78597f2494057c2d6fa1e781a74dbc2c5
dhruvdeshwal <b23mt1017@iitj.ac.in>
2026-02-26 23:29:50 +0530

    fix: use resend smtp

--- server/utils/mailSender.js
+++ server/utils/mailSender.js
@@ -15,7 +15,7 @@ const mailSender = async (email, title, body) => {
             })
 
             let info = await transporter.sendMail({
-                from: `"Knowledge Hub" <${process.env.MAIL_USER}>`,
+                from: 'Knowledge Hub <onboarding@resend.dev>',
                 to:`${email}`,
                 subject: `${title}`,
                 html: `${body}`,
